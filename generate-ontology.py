cd6c4a2cc88f327579c209989a7dbf87ee684fe4
Dylan Van Assche <dylan.vanassche@ugent.be>
2023-05-03 10:29:15 +0200

    add stubs for shapes and ontologies

---
+++ generate-ontology.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#
+# Generate the RML ontology from all its modules
+# Copyright (c) by Dylan Van Assche (2023)
+# IDLab - Ghent University - imec
+#
+
+def fetch_ontologies():
+    pass
+
+
+def save_ontologies():
+    pass
+
+
+if __name__ == '__main__':
+    ontologies = fetch_ontologies()
+    save_ontologies()
